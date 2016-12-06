5257bdc667dcb10438e7b04dcb7c34f311842182
LiuHaixin <liuhaixin2016@outlook.com>
2016-12-06 23:08:06 +0800

    Init

---
+++ Microsoft.AspNet.Identity/Microsoft.AspNet.Identity.Core/PasswordVerificationResult.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation, Inc. All rights reserved.
+// Licensed under the MIT License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNet.Identity
+{
+    public enum PasswordVerificationResult
+    {
+        Failed,
+        Success,
+        SuccessRehashNeeded
+    }
+}
